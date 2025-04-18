013c999d97259954e16cc73dcd24579c0f6ba942
isobrrj <igorsoaresoliver@hotmail.com>
2025-04-17 21:19:15 -0300

    ML4SMELLS-111 - new llm models

--- ml4smells-code-extractor.API/src/app/application/dtos/enums/llm_models.py
+++ ml4smells-code-extractor.API/src/app/application/dtos/enums/llm_models.py
@@ -4,4 +4,6 @@ class LLMModels(str, Enum):
     gemma2_2b = "gemma2:2b"
     gpt_4o_mini = "gpt-4o-mini"
     mistral_latest = "mistral:latest"
+    deepseek1_5b="deepseek-r1:1.5b"
+    qwen2_5_coder_1_5b="qwen2.5-coder:1.5b"
     gpt_4o      = "gpt-4o"
